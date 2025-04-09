92c76e79bce7be763b07279881efec8db447400e
Your Name <you@example.com>
2025-04-09 13:21:50 +0330

    https

--- src/API/weatherApi.tsx
+++ src/API/weatherApi.tsx
@@ -1,6 +1,6 @@
 export const FetchWeather = async () => {
   const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=714bb72dd4cf4e2c86475432251503&q=London&aqi=no"
+    "https://api.weatherapi.com/v1/current.json?key=714bb72dd4cf4e2c86475432251503&q=London&aqi=no"
   );
   if (!response.ok) {
     throw new Error("An Error occured in fetching weather");
